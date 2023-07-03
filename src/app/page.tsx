9ffbf2e348bcd2cddabbfcab869fd156cff5fe49
thomivargas <thvargas23@gmail.com>
2023-07-03 12:29:14 -0300

    boton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import CardImage from '@/components/CardImage'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       hola
+      <Link 
+        href="/blog"
+        className='text-blue-500 hover:text-blue-600 dark:hover:text-blue-400 dark:text-blue-400'
+      >
+        Blog
+      </Link>
     </main>
   )
 }
